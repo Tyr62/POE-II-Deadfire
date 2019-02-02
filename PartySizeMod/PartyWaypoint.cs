4655e5deb2b869ae3b4a34c0071d1358f12aa840
nbrunet <nbrunet62@gmail.com>
2019-02-02 15:59:14 +0100

    Update to version 1.3 Fix : - 6th character won't start scripted event due to this position (he try to move to the center of event point) - Can't climb the cliff in Beast Of Winter

---
+++ PartySizeMod/PartyWaypoint.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Game;
+using Onyx;
+using Patchwork;
+using UnityEngine;
+
+namespace PoE2Mods.PartySizeMod
+{
+    [ModifiesType]
+    public class mod_PartyWaypoint : PartyWaypoint
+    {
+        [ModifiesMember("GetMarkerPosition")]
+        public Vector3 GetMarkerPositionNew(PartyMember partyMember, bool reverse)
+        {
+            int absoluteFormationIndex = SingletonBehavior<PartyManager>.Instance.GetAbsoluteFormationIndex(partyMember);
+            bool secondary = partyMember.IsSecondaryPartyMember();
+            return GetMarkerPositionBySlot((!reverse) ? absoluteFormationIndex : (6 - absoluteFormationIndex), secondary);
+        }
+
+        [ModifiesMember("GetMarkerPositionBySlot")]
+        private Vector3 GetMarkerPositionBySlotNew(int slot, bool secondary)
+        {
+            if (slot >= 5)
+            {
+                if (Waypoints.Length - 1 < slot)
+                    Array.Resize(ref Waypoints, slot + 1);
+
+                if (Waypoints[slot] == null)
+                    Waypoints[slot] = new GameObject();
+
+                var neighbor = Waypoints[slot - 1];
+                var position = neighbor.transform.position + Waypoints[slot].transform.right;
+                var rotation = neighbor.transform.rotation;
+
+                Waypoints[slot].transform.SetPositionAndRotation(position, rotation);
+            }
+            
+            if (slot < 0 || slot >= Waypoints.Length || Waypoints[slot] == null)
+            {
+                return base.transform.position;
+            }
+
+            if (secondary)
+            {
+                Vector3 vector = Waypoints[slot].transform.position + Waypoints[slot].transform.right;
+                Vector3 origin = vector + Vector3.up * 2f;
+                RaycastHit result;
+                if (GameUtilities.Raycast(origin, Vector3.down, out result, float.PositiveInfinity, -5))
+                {
+                    vector = result.point;
+                }
+                return vector;
+            }
+
+            return Waypoints[slot].transform.position;
+        }
+    }
+}
--- PartySizeMod/UsableWithPartyMarkers.cs
+++ PartySizeMod/UsableWithPartyMarkers.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Game;
 using Game.AI;
 using Game.GameData;
@@ -11,40 +12,51 @@ namespace PoE2Mods.PartySizeMod
     [ModifiesType]
     public class mod_UsableWithPartyMarkers : UsableWithPartyMarkers
     {
-        //[ModifiesMember("CreatePartyMarkers")]
-        //public void CreatePartyMarkersNew()
-        //{
-        //    Vector2[] array = new Vector2[6]
-        //    {
-        //        new Vector2(-1f, -1.5f),
-        //        new Vector2(1f, -1.5f),
-        //        new Vector2(-1f, 0f),
-        //        new Vector2(1f, 0f),
-        //        new Vector2(-1f, 1.5f),
-        //        new Vector2(1f, 1.5f)
-        //    };
-        //    for (int i = 0; i < 6; i++)
-        //    {
-        //        GameObject gameObject = ResourceManager.CreateEmptyGameObject("pm" + (i + 1));
-        //        SceneTransitionMarker sceneTransitionMarker = ResourceManager.AddComponent<SceneTransitionMarker>(gameObject);
-        //        sceneTransitionMarker.slot = i;
-        //        gameObject.transform.parent = base.transform;
-        //        gameObject.transform.localPosition = new Vector3(array[i].x, 0f, array[i].y);
-        //        Vector3 navMeshPosition;
-        //        if (NavMeshUtility.GetPositionOnNavMesh(gameObject.transform.position, LayerUtility.WalkableLayersMask, -1, out navMeshPosition))
-        //        {
-        //            gameObject.transform.position = navMeshPosition;
-        //        }
-        //        PartyInteractionPoints[i] = gameObject.transform;
-        //    }
-        //}
-
         [ModifiesMember("GetMarkerPosition")]
         public Vector3 GetMarkerPositionNew(PartyMember partyMember, bool reverse)
         {
             int absoluteFormationIndex = SingletonBehavior<PartyManager>.Instance.GetAbsoluteFormationIndex(partyMember);
             bool secondary = partyMember.IsSecondaryPartyMember();
             return GetMarkerPositionBySlot((!reverse) ? absoluteFormationIndex : (6 - absoluteFormationIndex), secondary);
+        }
+
+        [ModifiesMember("GetMarkerPositionBySlot")]
+        private Vector3 GetMarkerPositionBySlotNew(int slot, bool secondary)
+        {
+            if (slot >= 5)
+            {
+                if (PartyInteractionPoints.Length - 1 < slot)
+                    Array.Resize(ref PartyInteractionPoints, slot + 1);
+
+                if (PartyInteractionPoints[slot] == null)
+                    PartyInteractionPoints[slot] = new GameObject().transform;
+                
+                var neighbor = PartyInteractionPoints[slot - 1];
+                var position = neighbor.transform.position + PartyInteractionPoints[slot].transform.right;
+                var rotation = neighbor.transform.rotation;
+
+                PartyInteractionPoints[slot].transform.SetPositionAndRotation(position, rotation);
+            }
+
+            if (PartyInteractionPoints == null || slot >= PartyInteractionPoints.Length || slot < 0 || PartyInteractionPoints[slot] == null)
+            {
+                return base.transform.position;
+            }
+
+            if (secondary)
+            {
+                Vector3 hitPosition = PartyInteractionPoints[slot].position + PartyInteractionPoints[slot].right;
+                Vector3 origin = hitPosition + Vector3.up * 2f;
+                RaycastHit result;
+                if (GameUtilities.Raycast(origin, Vector3.down, out result, float.PositiveInfinity, LayerUtility.WalkableLayersMask))
+                {
+                    hitPosition = result.point;
+                }
+                NavMeshUtility.SamplePosition(hitPosition, out hitPosition, 5f, NavMeshUtility.WalkableAreasMask);
+                return hitPosition;
+            }
+
+            return PartyInteractionPoints[slot].position;
         }
     }
 }
